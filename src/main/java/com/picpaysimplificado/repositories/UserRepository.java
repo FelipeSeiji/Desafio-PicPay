2d1e01456b848dd745dcc74425d5cccbc229021a
Felipe Seiji <fenishina@gmail.com>
2024-10-26 16:21:39 -0300

    Criação do UserService

--- src/main/java/com/picpaysimplificado/repositories/UserRepository.java
+++ src/main/java/com/picpaysimplificado/repositories/UserRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User,Long>{
     Optional<User> findUserByDocument(String document);
 
-    Optional<User> findUserById(String Id);
+    Optional<User> findUserById(Long Id);
 }
