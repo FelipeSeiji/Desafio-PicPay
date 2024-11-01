b7f63cf72517cdfcbc76f46e8fcb17bc8d3ed1c7
Felipe Seiji <fenishina@gmail.com>
2024-11-01 19:04:07 -0300

    Criação do TransactionController e correção de erros

--- src/main/java/com/picpaysimplificado/picpaysimplificado/service/TransactionService.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/service/TransactionService.java
@@ -26,29 +26,37 @@ public class TransactionService {
     @Autowired
     private RestTemplate restTemplate;
 
-    public void createTransaction(TransactionDTO transaction) throws Exception{
+    @Autowired
+    private NotificationService notificationService;
+
+    public Transaction createTransaction(TransactionDTO transaction) throws Exception{
         User sender = this.userService.findUserById(transaction.senderId());
         User receiver = this.userService.findUserById(transaction.receiverId());
 
         userService.validateTransaction(sender, transaction.value());
 
         boolean isAuthorized = this.authorizeTransaction(sender, transaction.value());
-        if(isAuthorized){
+        if(!isAuthorized){
             throw new Exception("Transação não autorizada");
         }
 
         Transaction newTransaction = new Transaction();
-        newTransaction.setAmount(newTransaction.getAmount());
+        newTransaction.setAmount(transaction.value());
         newTransaction.setSender(sender);
         newTransaction.setReceiver(receiver);
         newTransaction.setTimestamp(LocalDateTime.now());
 
         sender.setBalance(sender.getBalance().subtract(transaction.value()));
-        sender.setBalance(receiver.getBalance().add(transaction.value()));
+        receiver.setBalance(receiver.getBalance().add(transaction.value()));
 
         this.repository.save(newTransaction);
         this.userService.saveUser(sender);
         this.userService.saveUser(receiver);
+
+        this.notificationService.sendNotification(sender, "Transação realisada com sucesso");
+        this.notificationService.sendNotification(receiver, "Transação recebida com sucesso");
+        
+        return newTransaction;
     }
 
     public boolean authorizeTransaction(User sender, BigDecimal value){
